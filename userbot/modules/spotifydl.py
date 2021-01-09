aa0a30420fe5c6bd9fd11230d23a15552cdf3e05
4amparaboy <76533278+4amparaboy@users.noreply.github.com>
2021-01-09 23:28:32 +0700

    Update spotifydl.py

--- userbot/modules/spotifydl.py
+++ userbot/modules/spotifydl.py
@@ -10,7 +10,7 @@ from userbot import CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern=r"^\.sdd (.+?|)
+@register(outgoing=True, pattern="^.sdd")
 async def _(event):
 
     if event.fwd_from:
